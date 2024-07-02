bf4d250df85e1b1ef1a1cc6d6c3f051d027d605d
AUGUSTO FARIAS DOS SANTOS <139518502+AugustoAFS@users.noreply.github.com>
2024-07-02 20:46:23 -0300

    Add files via upload

---
+++ POO_Carro_Simples/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace POO_Carro_Simples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro myCar = new Carro();
+
+            Console.WriteLine("Digite a marca do carro:");
+            myCar.Marca = Console.ReadLine();
+
+            Console.WriteLine("Digite o modelo do carro:");
+            myCar.Modelo = Console.ReadLine();
+
+            Console.WriteLine("Digite a cor do carro:");
+            myCar.Cor = Console.ReadLine();
+
+            Console.WriteLine("Dados do carro:");
+            Console.WriteLine($"Marca: {myCar.Marca}");
+            Console.WriteLine($"Modelo: {myCar.Modelo}");
+            Console.WriteLine($"Cor: {myCar.Cor}");
+        }
+    }
+}
