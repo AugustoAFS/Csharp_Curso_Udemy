b29ae34a94357206b7e3d3b458769aebc446923e
AUGUSTO FARIAS DOS SANTOS <139518502+AugustoAFS@users.noreply.github.com>
2024-07-02 20:01:30 -0300

    Add files via upload

---
+++ POO_Curso_Csharp_Udemy/Carro.cs
@@ -0,0 +1,39 @@
+﻿namespace POO_Curso_Csharp_Udemy
+{
+    public class Carro
+    {
+        private string? Marca;
+        private string? Modelo;
+        private string? Cor;
+
+        public string? GetMarca()
+        {
+            return this.Marca;
+        }
+
+        public string? GetModelo()
+        {
+            return this.Modelo;
+        }
+
+        public string? GetCor()
+        {
+            return this.Cor;
+        }
+
+        public void SetMarca(string marca)
+        {
+            this.Marca = marca;
+        }
+
+        public void SetModelo(string modelo)
+        {
+            this.Modelo = modelo;
+        }
+
+        public void SetCor(string cor)
+        {
+            this.Cor = cor;
+        }
+    }
+}
---
+++ POO_Curso_Csharp_Udemy/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace POO_Curso_Csharp_Udemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro1 = new Carro();
+
+            Console.WriteLine("Informe a marca do carro:");
+            carro1.SetMarca(Console.ReadLine());
+
+            Console.WriteLine("Informe o modelo do carro:");
+            carro1.SetModelo(Console.ReadLine());
+
+            Console.WriteLine("Informe a cor do carro:");
+            carro1.SetCor(Console.ReadLine());
+
+            Console.WriteLine($"Marca: {carro1.GetMarca()}");
+            Console.WriteLine($"Modelo: {carro1.GetModelo()}");
+            Console.WriteLine($"Cor: {carro1.GetCor()}");
+        }
+    }
+}
